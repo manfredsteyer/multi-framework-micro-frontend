de10a3d2f316a2bde217c67062041620b09e2b67
Manfred Steyer <manfred.steyer@gmx.net>
2020-12-13 18:56:18 +0100

    corrected routes

--- mfe4-react/app.js
+++ mfe4-react/app.js
@@ -1,20 +1,27 @@
 import React from 'react'
-import ReactWebComponent from 'react-web-component';
+import ReactDOM from 'react-dom'
 
 class App extends React.Component {
+
   render() {
-    return (
-      <div>
+    const reactVersion = require('./package.json').dependencies['react'];
+
+    return ([
         <h1>
           Micro Frontend 4
-          <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" height="40"></img>
-        </h1>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" height="30"></img>
+        </h1>,
         <p>
-          Hello World!
+          React Version: {reactVersion}
         </p>
-     </div>
-    )
+    ])
   }
 }
 
-ReactWebComponent.create(<App />, 'mfe4-element');
+class Mfe4Element extends HTMLElement {
+  connectedCallback() {
+    ReactDOM.render(<App/>, this);
+  }
+}
+
+customElements.define('mfe4-element', Mfe4Element);
--- shell/src/app/app.module.ts
+++ shell/src/app/app.module.ts
@@ -12,9 +12,9 @@ import { WrapperComponent } from './wrapper/wrapper.component';
     BrowserModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { matcher: startsWith('mfe1/a'), component: WrapperComponent, data: { importName: 'mfe1', elementName: 'mfe1-element' }},
-      { matcher: startsWith('mfe2/a'), component: WrapperComponent, data: { importName: 'mfe2', elementName: 'mfe2-element' }},
-      { matcher: startsWith('mfe3/a'), component: WrapperComponent, data: { importName: 'mfe3', elementName: 'mfe3-element' }},
+      { matcher: startsWith('mfe1'), component: WrapperComponent, data: { importName: 'mfe1', elementName: 'mfe1-element' }},
+      { matcher: startsWith('mfe2'), component: WrapperComponent, data: { importName: 'mfe2', elementName: 'mfe2-element' }},
+      { matcher: startsWith('mfe3'), component: WrapperComponent, data: { importName: 'mfe3', elementName: 'mfe3-element' }},
       { matcher: startsWith('mfe4'), component: WrapperComponent, data: { importName: 'mfe4', elementName: 'mfe4-element' }},
 
     ])
